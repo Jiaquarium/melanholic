c45e39f2defc5d0b26381d265e238f10e02e21ba
James Gu <jjg@issuu.com>
2020-08-02 19:59:37 -0500

    Layout basic next js frameworkt

---
+++ nextjs-blog/lib/posts-data.js
@@ -0,0 +1,49 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+const postsDirectory = path.join(process.cwd(), 'posts-data');
+
+function getAllFileData(fileNames)
+{
+    return fileNames.map(fileName => {
+        return new Promise((resolve, reject) =>
+        {
+            // remove md from fileName to get Id
+            const Id = fileName.replace(/\.md$/, '');
+            
+            // read markdown as string
+            const fullPath = path.join(postsDirectory, fileName);
+            return fs.readFile(fullPath, {encoding: 'utf8'}, (err, fileContents) => {
+                // use gray-matter to parse through YAML "front-matter"
+                console.log("fileContents" + fileContents);
+                const matterResult = matter(fileContents);
+        
+                // combine the data with Id
+                if (err)
+                {
+                    return reject(err);
+                }
+                else
+                {
+                    return resolve({
+                        Id,
+                        ...matterResult.data
+                    });
+                };
+            });
+        })
+    });
+}
+
+export async function getSortedPostsData() {
+    // get file names under post
+    return new Promise(resolve => fs.readdir(postsDirectory, (err, fileNames) => {
+        Promise.all(
+            getAllFileData(fileNames)
+        ).then(allPostData => resolve(allPostData.sort((a, b) => a.date < b.date ? 1 : -1))
+        ).catch(err => {
+            console.error("something went wrong");
+        })
+    }));
+};
